259c4e0691416d099a8694b4b7d68d90da17f2c1
H82M19\Student <Student>
2026-03-18 11:14:38 +0700

    feat(week05:basic-routing): adding 404 page for the wrong url

--- Week05/HelloRouter/src/App.tsx
+++ Week05/HelloRouter/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
+import NotFound from './components/NotFound'
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/contact' element={<Contact/>}/>
-
+        <Route path='*' element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
   )
